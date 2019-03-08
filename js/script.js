102cfdd247274c8276ee0c8109f53c6063c63974
Slavik <slavikaremenko@gmail.com>
2019-03-08 13:41:56 +0200

    Add countint script to quick-view popup

--- js/script.js
+++ js/script.js
@@ -5,4 +5,24 @@ $(document).ready(function(){
     slidesToShow: 4,
     slidesToScroll: 1
   });
+
+  // Quantity of products in quick view popup
+  (function quantityProducts() {
+    var $quantityArrowMinus = $(".quantity__minus");
+    var $quantityArrowPlus = $(".quantity__plus");
+    var $quantityNum = $(".quantity__number-input");
+ 
+    $quantityArrowMinus.click(quantityMinus);
+    $quantityArrowPlus.click(quantityPlus);
+ 
+    function quantityMinus() {
+      if ($quantityNum.val() > 1) {
+        $quantityNum.val(+$quantityNum.val() - 1);
+      }
+    }
+ 
+    function quantityPlus() {
+      $quantityNum.val(+$quantityNum.val() + 1);
+    }
+  })();
 });
