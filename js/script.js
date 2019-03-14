127340653f843144519ebe662492b864a22fe417
Bibliaricus <slavikdev95@gmail.com>
2019-03-14 13:10:25 +0200

    Responsive: Category slider (768-991)

--- js/script.js
+++ js/script.js
@@ -3,7 +3,33 @@ $(document).ready(function(){
   $('.categories').slick({
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
   // Quantity of products in quick view popup
